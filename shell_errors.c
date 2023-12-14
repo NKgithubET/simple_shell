378bbac795366e552002827a6d10832c3fa46514
NK <110395153+NKgithubET@users.noreply.github.com>
2023-12-14 03:40:15 +0300

    verified_commit

--- shell_errors.c
+++ shell_errors.c
@@ -1,8 +1,6 @@
 #include "shell.h"
 
 /**
- *_eputs - prints an input string
- * @str: the string to be printed
  *
  * Return: Nothing
  */
@@ -23,8 +21,6 @@ void _eputs(char *str)
  * _eputchar - writes the character c to stderr
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 int _eputchar(char c)
 {
@@ -42,9 +38,6 @@ int _eputchar(char c)
 }
 
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -67,9 +60,6 @@ int _putfd(char c, int fd)
 /**
  *_putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
  */
 int _putsfd(char *str, int fd)
 {
