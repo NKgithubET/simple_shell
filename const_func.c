19fa82bcc83627625569d2d3d59f985851a401ec
NK <110395153+NKgithubET@users.noreply.github.com>
2023-12-14 03:33:58 +0300

    verified_commit

--- const_func.c
+++ const_func.c
@@ -1,9 +1,6 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
@@ -31,9 +28,8 @@ int _myexit(info_t *info)
 
 /**
  * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * 
+ *  
  */
 int _mycd(info_t *info)
 {
@@ -80,8 +76,8 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
+ *
+ * 
  *          constant function prototype.
  *  Return: Always 0
  */
